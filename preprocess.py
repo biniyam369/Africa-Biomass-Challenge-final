391120f8c48ef5711c297a1807e0b4178abe0738
biniyam369 <binadsci@gmail.com>
2023-04-23 00:10:13 +0000

    final fix on  preprocess

--- preprocess.py
+++ preprocess.py
@@ -19,6 +19,7 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset,TensorDataset, DataLoader,RandomSampler
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 import argparse
+import gc
 
 def Parse_args():
     args = argparse.ArgumentParser()
@@ -33,8 +34,11 @@ args = Parse_args()
 
 warnings.simplefilter('ignore')
 
-ncompo_genes = 80
-ncompo_cells = 10
+data_dir = Path('data')
+data_dir.mkdir(exist_ok=True)
+
+train_path = data_dir / "processed"
+train_path.mkdir(exist_ok=True)
 
 def Seed_everything(seed=369):
     random.seed(seed)
@@ -68,20 +72,14 @@ trainset = h5py.File(train_files[0], "r")
 validset = h5py.File(train_files[1], "r")
 validset2 = h5py.File(train_files[2], "r")
 
-train_images = np.array(trainset['images'],dtype=np.float32)
-valid_images = np.array(validset['images'],dtype=np.float32)
-valid_images2 = np.array(validset2['images'], dtype=np.float32)
-
-train_biomasses = np.array(trainset['agbd'],dtype=np.float32)
-valid_biomasses = np.array(validset['agbd'],dtype=np.float32)
-valid_biomasses2 = np.array(validset2['agbd'],dtype=np.float32)
-
-
-print(f"train dataset size {train_images.shape} train lab size {train_biomasses.shape}")
-print(f"validate dataset size {valid_images.shape} validate lab size {valid_biomasses.shape}")
-print(f"validate dataset2 size {valid_images2.shape} test lab size {valid_images2.shape}")
-print(f"test dataset size {test_images.shape}")
-
+train_data ={}
+for col in ['agbd', 'cloud', 'images', 'lat', 'lon', 'scl']:
+    trn_temp = np.array(trainset[col], dtype=np.float32)
+    vld_temp = np.array(validset[col],dtype=np.float32)
+    vld_temp2 = np.array(validset2[col],dtype=np.float32)
+    train_data[col] = np.concatenate([trn_temp, vld_temp, vld_temp2])
+    
+    
 def Feature(df):
     transformers = {}
     for i in range(12):
@@ -92,3 +90,50 @@ def Feature(df):
         transformers[i] = transformer
         
     return df, transformers
+
+train_images = train_data['images']
+
+tt = np.concatenate([train_images, test_images])
+tt, transformers = Feature(tt)
+train_images = tt[:train_images.shape[0]]
+test_images = tt[train_images.shape[0]:]
+
+train_imagee = pd.DataFrame(train_data['images'].reshape(-1, 15*15*12))
+train_imagee["ID"] = train_imagee.index.values
+
+train_agbd = pd.DataFrame(train_data['agbd'].reshape(-1, 1)).rename(columns={0:'agbd'})
+train_cloud = pd.DataFrame(train_data['cloud'].reshape(-1, 15*15))
+train_cloud.rename(columns={i:f"cld-{i}" for i in train_cloud.columns}, inplace=True)
+
+train_lat = pd.DataFrame(train_data['lat'].reshape(-1, 15*15))
+train_lat.rename(columns={i:f"lat-{i}" for i in train_lat.columns}, inplace=True)
+
+train_lon = pd.DataFrame(train_data['lon'].reshape(-1, 15*15))
+train_lon.rename(columns={i:f"lon-{i}" for i in train_lon.columns}, inplace=True)
+
+train_scl = pd.DataFrame(train_data['scl'].reshape(-1, 15*15))
+train_scl.rename(columns={i:f"scl-{i}" for i in train_scl.columns}, inplace=True)
+
+train_gt = pd.concat([train_agbd, train_cloud, train_lon, train_lat, train_scl], axis=1)
+train_gt["ID"] = train_imagee.index.values
+
+print(train_imagee.shape, train_cloud.shape,train_lat.shape,train_lon.shape,train_scl.shape,)
+
+
+del train_cloud, train_lat, train_lon, train_scl, train_agbd
+gc.collect()
+
+train_imagee.loc[:,'fold'] = -1
+train_gt.loc[:,'fold'] = -1
+
+skf = MultilabelStratifiedKFold(n_splits = 5, shuffle=True, random_state=369)
+for fold, (trn_index, val_index) in enumerate(skf.split(train_imagee,  train_gt)):
+    train_imagee.loc[val_index,'fold'] = fold
+    train_gt.loc[val_index,'fold'] = fold
+    
+    train_imagee.to_csv(f'./data/processed/train_images_{fold}.csv', index=False)
+    train_gt.to_csv(f'./data/processed/train_gt_fld{fold}.csv', index=False)
+    
+    train_imagee.loc[:,'fold'] = -1
+    train_gt.loc[:,'fold'] = -1
+   
